e429305baf152c0cd6155c472a3e1cbe1b1536ba
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-08-25 10:01:17 +0530

    done2

--- src/Style/button.js
+++ src/Style/button.js
@@ -2,22 +2,19 @@ import React, {useState, Component} from 'react'
 import FuncBased from './FuncBased'
 import ClassFunc from './ClassBased'
 
+
 function Buttonone(){
     const [count, setCount] = useState(false)
     console.log(count)
     return(
         <div className='btn1'>
-            <h1 className='mainheading'>PREPBYTES</h1>
             <h1>{count}</h1>
-            <button onClick={() => setCount(!count)}>To see Style on function</button>
+            <button onClick={() => setCount(!count)} className='btn1'>To see Styling in functional component</button>
             {count ? <FuncBased></FuncBased>: null}
         </div>
     )
 }
-
 export default Buttonone
-
-
 
 class Buttontwo extends Component {
     constructor(){
@@ -29,12 +26,11 @@ class Buttontwo extends Component {
     }
     render(){
         return(
-            <div className='btn2'>
-                <button onClick={() => this.setState({buttonValue: !this.state.buttonValue})}>Class button</button>
+            <div className=''>
+                <button onClick={() => this.setState({buttonValue: !this.state.buttonValue})} className='btn2'>To see styling in class component</button>
                 {this.state.buttonValue ? null : <ClassFunc />}
             </div>
         )
     }
 }
-
 export {Buttontwo}
